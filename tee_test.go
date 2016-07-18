d183cb25a053a2c0a45d4711ff89923efc856348
Bruno Panuto <thepanuto@gmail.com>
2016-07-18 16:32:39 -0300

    Added code for checking against HTML messages

--- tee_test.go
+++ tee_test.go
@@ -1,6 +1,9 @@
 package tts
 
-import "testing"
+import (
+	"bytes"
+	"testing"
+)
 
 func TestSpeak(t *testing.T) {
 	speech, err := Speak(Config{Language: "en-US", Speak: "hello"})
@@ -11,5 +14,8 @@ func TestSpeak(t *testing.T) {
 	audio := speech.Bytes()
 	if len(audio) == 0 {
 		t.Error("Should not return an empty audio")
+	}
+	if bytes.Contains(audio, []byte("HTML")) {
+		t.Error("Should not have any HTML related code")
 	}
 }
